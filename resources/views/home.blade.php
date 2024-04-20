d4a915e5cca60fa9aa6600b4f7da3258ea66d25b
scarlett-troncoso <scar.labussieri@gmail.com>
2024-04-20 17:11:45 +0200

    add images comics

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Home Page</h1>
+    <section id="comics">
 
-    @foreach ($comics as $comic)
-        <div>
-            {{ $comic['title'] }}
+        <div class="jumbotron"> </div>
+
+        <div class="container">
+            <!-- copertine dei comics -->
+            <div class="row">
+                @foreach ($comics as $comic)
+                    <div class="col-2">
+                        <div class="card">
+                            <img src="{{ $comic['thumb'] }}" alt="" class="cover-comic">
+                            <span>{{ $comic['series'] }}</span>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+
+            <button>LOAD MORE</button>
+
         </div>
-    @endforeach
+    </section>
+
+    <section id="menu-main">
+        <div class="nav-main">
+            <div class="container">
+                <nav>
+                    <a href="/">DIGITAL COMICS</a>
+
+                    <a href="/">DC MERCHANDISE</a>
+                    <!--class, se mi restituisce l'estringa 'products' allora vuol dire che la route ativa é questa e la voglio attivare-->
+                    <!-- se "Route::currentRouteName()" mi restituisce una sringa con il nome della Route che in questo caso é comics (questa Route é in web.php)-->
+
+                    <a href="">SUBSCRIPTION</a>
+
+                    <a href="/">COMIC SHOP LOCATOR</a>
+
+                    <a href="/">DC POWER VISA</a>
+                </nav>
+            </div>
+        </div>
+    </section>
 @endsection
